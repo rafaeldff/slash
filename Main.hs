7e2807b53f464aef010f0308ebb7a3e0225d98bb
Rafael de F. Ferreira <public@rafaelferreira.net>
2014-11-24 23:29:53 -0200

    Prompting for basic command

--- Main.hs
+++ Main.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Network.Wreq
 import Control.Lens
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as C
+
+fetch :: String -> Response ByteString -> ByteString
+fetch "body"   response = response ^. responseBody
+fetch "status" response = C.pack $ show $ (response ^. responseStatus . statusCode)
+fetch _ response        = "<error>"
 
 main = do
   putStrLn "uri: "
   uri <- getLine
   response <- get uri
-  putStrLn "response body: "
-  putStrLn $ show (response ^. responseBody)
+  putStrLn "body or status?: "
+  command <- getLine
+  putStrLn $ show $ fetch command response
   main
