48b047ec8e8afc06042c410ef30e275a3d13dc34
Rafael de F. Ferreira <public@rafaelferreira.net>
2014-11-27 14:21:15 -0200

    Decent prompt string

--- Main.hs
+++ Main.hs
@@ -8,6 +8,8 @@ import Text.Parsec.ByteString.Lazy
 import Network.Wreq
 import Control.Lens
 
+import System.IO (hFlush,stdout)
+
 data Command = Body | Status
                deriving (Eq, Show)
 
@@ -28,11 +30,14 @@ status  = do _ <- string "status"
 body    = do _ <- string "body"
              return Body
 
+prompt str = do putStr str
+                hFlush stdout
+
 main = do
-  Prelude.putStrLn "uri: "
+  prompt "slash> "
   uri <- getLine
   response <- get uri
-  Prelude.putStrLn "body or status?: "
+  prompt "slash> "
   command <- getLine
   Prelude.putStrLn $  maybe "error" show (fmap (execute response) (parseCommand command))
   main
